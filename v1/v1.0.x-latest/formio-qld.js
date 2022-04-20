d74dbc2d16b81d8a809f041ba4ce2a209f13b9ee
devonpis <devonpis@users.noreply.github.com>
2022-04-20 06:34:24 +0000

    [Build and Test] Publish
    from qld-gov-au/formio:v1.0.4-beta003/dist REV:ea8ccbec46cb078c3610dc37c1211f8afb96b1d2
    
    debug plsplusaddress saving issue

--- v1/v1.0.x-latest/formio-qld.js
+++ v1/v1.0.x-latest/formio-qld.js
@@ -17739,6 +17739,7 @@ __webpack_require__.r(__webpack_exports__);
 var components_namespaceObject = {};
 __webpack_require__.r(components_namespaceObject);
 __webpack_require__.d(components_namespaceObject, {
+  "CustomAddress": () => (CustomAddress),
   "CustomHeader": () => (CustomHeader),
   "CustomTextfield": () => (CustomTextfield),
   "PlsPlusAddress": () => (PlsPlusAddress)
@@ -18529,7 +18530,35 @@ class PlsPlusAddress extends ContainerComponent {
 PlsPlusAddress.editForm = PlsPlusAddress_form;
 ;// CONCATENATED MODULE: ./src/components/PlsPlusAddress/index.js
 
+;// CONCATENATED MODULE: ./src/components/CustomAddress/CustomAddress.js
+const AddressComponent = Formio.Components.components.address;
+class CustomAddress extends AddressComponent {
+  /**
+   * Define the default schema to change the type and tag and label.
+   */
+  static schema(...extend) {
+    return AddressComponent.schema({
+      label: "CustomAddress",
+      type: "customaddress"
+    }, ...extend);
+  }
+
+  static get builderInfo() {
+    return {
+      title: "CustomAddress",
+      group: "custom",
+      icon: "terminal",
+      weight: 2,
+      documentation: "/userguide/#html-element-component",
+      schema: CustomAddress.schema()
+    };
+  }
+
+}
+;// CONCATENATED MODULE: ./src/components/CustomAddress/index.js
+
 ;// CONCATENATED MODULE: ./src/components/index.js
+
 
 
 
