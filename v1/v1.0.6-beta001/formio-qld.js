5fa6cdc689fb9dbec3b334428d26fb4119bccead
devonpis <devonpis@users.noreply.github.com>
2022-05-20 00:51:45 +0000

    [Build and Test] Publish
    from qld-gov-au/formio:v1.0.6-beta001/dist REV:184ce03dc7e1e666ee2f0feb9c984ebbc4d2aaeb
    
    Merge branch 'PlsPlusAddress-hotfix' into QOL-8273-create-PdfSubmitButton-custom-component

--- v1/v1.0.6-beta001/formio-qld.js
+++ v1/v1.0.6-beta001/formio-qld.js
@@ -18015,17 +18015,6 @@ class PlsPlusAddress extends FieldsetComponent {
     });
   }
 
-  addComponents(data = this.data, options = this.options) {
-    if (options.components) {
-      this.components = options.components;
-    } else {
-      console.log("test444", this);
-      if (this.attached) this.prepComponents(this.componentComponents);
-      const components = this.hook("addComponents", lodash_default().defaultsDeep(this.componentComponents, this.defaultSchema.components), this) || [];
-      components.forEach(component => this.addComponent(component, data));
-    }
-  }
-
   mergeSchema(component = {}) {
     let {
       defaultSchema
@@ -18255,7 +18244,7 @@ class PlsPlusAddress extends FieldsetComponent {
 
   renderElement(value) {
     this.container?.getComponents().forEach(component => {
-      if (!this.builderMode) {
+      if (!this.builderMode && this.attached) {
         component.disabled = component.originalComponent.disabled || !this.manualMode;
         component.component.validate = !this.manualMode ? {} : component.originalComponent.validate;
       }
@@ -18269,7 +18258,7 @@ class PlsPlusAddress extends FieldsetComponent {
       };
     });
 
-    if (!this.builderMode) {
+    if (!this.builderMode && this.attached) {
       this.component.validate = { ...(this.originalComponent?.validate?.required && {
           custom: `valid = !!instance.address.selectedAddress;`,
           customMessage: `${this.component.label} is required.`,
